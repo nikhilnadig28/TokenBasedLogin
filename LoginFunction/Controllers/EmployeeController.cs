fba02f4101d773abc75d99fc27f975dbda27273a
nikhilnadig28 <nikhilnadig28@gmail.com>
2017-03-14 09:59:36 +0530

    Initial Commit

---
+++ LoginFunction/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoginFunction.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<TBL_EMPLOYEES> Get()
+        {
+            using (dbNEVSEntities entities = new dbNEVSEntities())
+            {
+                return entities.TBL_EMPLOYEES.ToList();
+            }
+        }
+    }
+}
